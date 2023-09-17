fee5fc27416c18cf3cbcb80ad59006a50833079b
justin-owen <justinowen22345@gmail.com>
2023-09-16 22:42:31 -0400

    part 5 done

--- src/test/java/EducationServiceTest.java
+++ src/test/java/EducationServiceTest.java
@@ -1,3 +1,22 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static sun.nio.cs.Surrogate.is;
+
 public class EducationServiceTest {
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -13,4 +32,47 @@ public class EducationServiceTest {
 
     @InjectMocks
     private ClientBusinessObjectImpl clientBusinessObjectImpl;
+
+    @Test
+    public void testStudentsByClass() {
+
+        Student studentJM = new Student(“Joy Ma”, “Spanish);
+        Student studentJH = new Student(“Julio Hernandez”, “Algebra”);
+        Student studentJJ = new Student(“Jenny Jones”, “Calculus”);
+        List<Student> allStudents = Arrays.asList(studentJM, studentJH, studentJJ);
+
+        given(studentDataObject.getAllStudents()).willReturn(allStudents);
+
+
+        List<String> mathStudents = clientBusinessObjectImpl.getAllStudentsBySubject(“math”);
+
+        // Then
+        assertThat(mathStudents.size(), is(2));
+        assertThat(mathStudents, hasItems(studentJJ, student JH);
+    }
+    // First: Setup
+
+    @Captor
+    ArgumentCaptor<Class> classArgumentCaptor;
+
+// Next:
+
+    @Test
+    public void testMarkInactive_argumentCaptor() {
+        // Given
+        Class geometry = new Class("Geometry", "Summer 2022");
+        Class envSci = new Class("Environmental Science", "Fall 2022");
+        Class compLit = new Class("Comparative Literature", "Spring 2023");
+        List<Class> allClasses = Arrays.asList(geometry, envSci, compLit);
+
+        given(classDataObject.getAllClasses()).willReturn(allClasses);
+
+        // When
+        clientBusinessObjectImpl.markCurrentClassesInactive();
+
+        // Then
+        verify(classDataObject).markInactive(classArgumentCaptor.capture());
+
+        assertEquals(geometry, classArgumentCaptor.getValue());
+    }
 }
